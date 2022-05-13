819c511528c1c9503c452c1cd04d511c5230d702
Fernanda Reis Kennedy <fernandarkennedy@Fernandas-MacBook-Pro.local>
2022-05-13 07:44:34 -0400

    New places can be Add

--- controllers/places.js
+++ controllers/places.js
@@ -1,25 +1,47 @@
 const router = require('express').Router()
-
+const places = require('../models/places.js')
 
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: 'https://www.lacademie.com/wp-content/uploads/2021/08/various-asian-meals.jpg'
-    },
-    
-   {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: 'https://travel.home.sndimg.com/content/dam/images/travel/fullrights/2019/4/22/0/CI_Catmosphere-Laguna_coffee-bar.jpg.rend.hgtvcom.966.644.suffix/1555930348188.jpeg'
-    }]
     res.render('places/index', { places })
 })
 
+
+// router.get('/', (req, res) => {
+//     let places = [{
+//         name: 'H-Thai-ML',
+//         city: 'Seattle',
+//         state: 'WA',
+//         cuisines: 'Thai, Pan-Asian',
+//         pic: 'https://www.lacademie.com/wp-content/uploads/2021/08/various-asian-meals.jpg'
+//     },
+    
+//    {
+//         name: 'Coding Cat Cafe',
+//         city: 'Phoenix',
+//         state: 'AZ',
+//         cuisines: 'Coffee, Bakery',
+//         pic: 'https://travel.home.sndimg.com/content/dam/images/travel/fullrights/2019/4/22/0/CI_Catmosphere-Laguna_coffee-bar.jpg.rend.hgtvcom.966.644.suffix/1555930348188.jpeg'
+//     }]
+//     res.render('places/index', { places })
+// })
+
+router.post('/', (req, res) => {
+
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
+  })
+  
+  
 router.get('/new', (req, res) => {
     res.render('places/new')
   })
